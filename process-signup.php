48df8015fe807217e33f167a247e59c114b6cca8
leilaLAG <laila.lagnaoui21@gmail.com>
2023-10-17 20:40:56 +0100

    Your commit message

---
+++ process-signup.php
@@ -0,0 +1,9 @@
+<?php
+if(empty($_POST["name"])){
+   die("Name is required");
+}
+ if ( ! filter_var($_POST["email"],FILTER_VALIDATE_EMAIL)){
+    die("Valid email is required");
+ }
+
+print_r($_POST);
